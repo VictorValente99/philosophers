dd78d35c688e70d4a45a997610de7c0755d62c69
Victor <vitorvallete@gmail.com>
2026-06-17 16:16:40 -0300

    feat: libs and makefile

---
+++ src/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/17 16:13:28 by victde-s          #+#    #+#             */
+/*   Updated: 2026/06/17 16:15:42 by victde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <limits.h>
