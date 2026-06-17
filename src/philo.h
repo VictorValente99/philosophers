f46bc792868dee5d8ee1898037da4fefa1326639
Victor <vitorvallete@gmail.com>
2026-06-17 17:51:46 -0300

    feat: data structs

--- src/philo.h
+++ src/philo.h
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 16:13:28 by victde-s          #+#    #+#             */
-/*   Updated: 2026/06/17 16:15:42 by victde-s         ###   ########.fr       */
+/*   Updated: 2026/06/17 17:51:08 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,36 @@
 #include <stdbool.h>
 #include <sys/time.h>
 #include <limits.h>
+
+typedef	pthread_mutex_t t_mtx;
+
+typedef struct s_fork
+{
+	t_mtx		fork;
+	int			fork_id;
+}	t_fork;
+
+typedef	struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+}	t_philo;
+
+typedef struct s_table
+{
+	long		philos_nbr;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		nbr_limit_meals;
+	long		start_simulation;
+	bool		end_simulation;
+	t_philo		*philos;
+	t_fork		*forks;
+	t_mtx		print_mutex;
+}	t_table;
