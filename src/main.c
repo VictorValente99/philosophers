b3d01d9bc1f254b5f4f679a7eb1728bfd4601e6b
Victor <vitorvallete@gmail.com>
2026-06-18 14:27:27 -0300

    feat: add pilot main.c with error handler function

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include "philo.h"
+
+int	main(int ac, char **av)
+{
+	if (ac == 5 || ac == 6)
+	{
+		//correct number of arguments, we can initialize the table struct
+		//1) errors checking, filling the table
+		parse_input(&table, av);
+		//2) initialize the mutexes and the philosophers
+		data_init(&table);
+		//3) start the simulation
+		dinner_time(&table);
+		//4) free the memory and destroy the mutexes
+		clean(&table);
+	}
+	else
+	{
+		error_exit("Wrong input.");
+		return (1);
+	}
+}
